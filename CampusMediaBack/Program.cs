8cb2c04fbf260d47e962c6968edc14b85ed9fb89
Xhoana Thano <xhoanathano@gmail.com>
2026-01-31 14:59:45 +0100

    formatting code

--- CampusMediaBack/Program.cs
+++ CampusMediaBack/Program.cs
@@ -10,7 +10,6 @@ using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
